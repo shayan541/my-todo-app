0843d046329b1856e1c505d423edb8ee123af928
shayan karampisheh <shayan.karampisheh@gmail.com>
2025-09-13 20:01:07 +0430

    fix: fix bugs

--- src/componnets/common/FormModal.tsx
+++ src/componnets/common/FormModal.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Modal from "./Modal";
-import type { FormModalProps } from "../../../types/popup";
-import Form from "../../todoList/form/Form";
+import type { FormModalProps } from "../../types/popup";
+import Modal from "../ui/Modal";
+import Form from "../todoList/form/Form";
 
 const FormModal: React.FC<FormModalProps> = ({ isShown, onSubmit, onEdit, onClose, data }) => {
   return (
