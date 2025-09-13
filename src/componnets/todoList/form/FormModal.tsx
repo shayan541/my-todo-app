e022937f1f9d295c6e631bfae125ccefe63fc438
shayan karampisheh <shayan.karampisheh@gmail.com>
2025-09-13 20:28:45 +0430

    fix: fix folder and color

--- src/componnets/todoList/form/FormModal.tsx
+++ src/componnets/todoList/form/FormModal.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import type { FormModalProps } from "../../types/popup";
-import Modal from "../ui/Modal";
-import Form from "../todoList/form/Form";
+import type { FormModalProps } from "../../../types/popup";
+import Modal from "../../ui/Modal";
+import Form from "./Form";
 
 const FormModal: React.FC<FormModalProps> = ({ isShown, onSubmit, onEdit, onClose, data }) => {
   return (
     <Modal isShown={isShown} onClose={onClose}>
       <div className="flex flex-col bg-white rounded shadow py-8 px-6 border border-gold-200">
-        <h2 className="font-bold">{data ? "Edit Task" : "Add New Task"}</h2>
+        <h2 className="font-bold text-black">{data ? "Edit Task" : "Add New Task"}</h2>
         <Form onSubmit={onSubmit} data={data} onEdit={onEdit} />
       </div>
     </Modal>
