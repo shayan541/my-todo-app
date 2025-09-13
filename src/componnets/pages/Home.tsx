ae2abcae87c14ae5441f69c28f318bbf7c574883
shayan karampisheh <shayan.karampisheh@gmail.com>
2025-09-13 20:19:06 +0430

    fix: fix color of texts

--- src/componnets/pages/Home.tsx
+++ src/componnets/pages/Home.tsx
@@ -3,7 +3,7 @@ import TodoList from "../todoList/TodoList";
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <h1 className="text-black">Home</h1>
       <TodoList />
     </div>
   );
