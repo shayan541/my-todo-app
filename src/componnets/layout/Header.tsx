d21020de1f163ca0048d07577a7ce85ae34274d0
shayan karampisheh <shayan.karampisheh@gmail.com>
2025-09-15 19:50:56 +0430

    fix: remove hover color

--- src/componnets/layout/Header.tsx
+++ src/componnets/layout/Header.tsx
@@ -5,11 +5,11 @@ const Header = () => {
     <header className="bg-gold-100">
       <div className="max-w-[1440px] h-12 mx-auto flex items-center px-4">
         <nav>
-          <Link to="/" className="hover:text-gold-200 text-black mr-2">
+          <Link to="/" className=" text-black mr-2">
             Home
           </Link>
           |
-          <Link to="/about" className="hover:text-gold-200 text-black ml-2">
+          <Link to="/about" className=" text-black ml-2">
             About
           </Link>
         </nav>
